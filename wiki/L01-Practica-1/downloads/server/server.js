290688d282702735720a6e3c0e7b5792a73274ec
obijuan <juan@iearobotics.com>
2019-01-27 13:22:42 +0100

    L01: server.js updated

--- wiki/L01-Practica-1/downloads/server/server.js
+++ wiki/L01-Practica-1/downloads/server/server.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
+console.log("Arrancando servidor...")
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
+  console.log("Peticion atendida")
 }).listen(8080);
